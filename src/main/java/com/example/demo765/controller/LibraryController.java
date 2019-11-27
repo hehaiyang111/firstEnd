5fe629feb2c5946784b6386406ef65f6aadeccc6
hehaiyang111 <1464751753@qq.com>
2019-11-27 15:49:15 +0800

    图书馆部分的crud

--- src/main/java/com/example/demo765/controller/LibraryController.java
+++ src/main/java/com/example/demo765/controller/LibraryController.java
@@ -1,9 +1,11 @@
 package com.example.demo765.controller;
 
+import com.alibaba.fastjson.JSONObject;
 import com.example.demo765.entity.Book;
 import com.example.demo765.entity.Search;
 import com.example.demo765.entity.User;
 import com.example.demo765.mapper.BookMapper;
+import com.example.demo765.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,11 +17,16 @@ import java.util.List;
 public class LibraryController {
     @Autowired
     BookMapper bookMapper;
-
+    @Autowired
+    BookService bookService;
+    JSONObject jsonObject = new JSONObject();
 
     @GetMapping("/findAllBook")
     public List<Book> findAllBook(){
+//        jsonObject.put("data",bookMapper.findAll());
         return bookMapper.findAll();
+
+//        return jsonObject;
     }
 
     @CrossOrigin
@@ -50,5 +57,10 @@ public class LibraryController {
         int id = user.getId();
         System.out.printf("id:" +id);
         bookMapper.deleteByPrimaryKey(id);
+    }
+
+    @RequestMapping("/addorupdatebook")
+    public int addOrUpdateBook(@RequestBody Book book){
+        return bookService.addOrUpdate(book);
     }
 }
--- src/main/java/com/example/demo765/entity/Book.java
+++ src/main/java/com/example/demo765/entity/Book.java
@@ -9,6 +9,15 @@ public class Book {
 
     private String date;
 
+    public String getTypeName() {
+        return typeName;
+    }
+
+    public void setTypeName(String typeName) {
+        this.typeName = typeName;
+    }
+
+    private String typeName;
     public Integer getCid() {
         return cid;
     }
